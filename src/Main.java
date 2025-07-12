2c148ad34e0cae370189cb0dba4e58c1cfc09240
Sovanroth13 <sovanroth016@gmail.com>
2025-07-12 14:32:02 +0700

    udpate source code

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         try {
-            int[] sizes = {100000000};
+            int[] sizes = {100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 500000000 };
 
             for (int size : sizes) {
                 System.out.println("\n============ SORTING " + size + " Array ============");
